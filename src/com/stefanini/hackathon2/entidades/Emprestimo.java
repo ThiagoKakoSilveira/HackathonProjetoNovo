9a7ba396e6dcd377b476b4cac54786ec60b4922c
Thiago Machado Silveira <tmsilveira@stefanini.com>
2016-04-08 19:14:22 -0300

    push com entidade emprestimo criada mas nada feito

---
+++ src/com/stefanini/hackathon2/entidades/Emprestimo.java
@@ -0,0 +1,20 @@
+package com.stefanini.hackathon2.entidades;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Emprestimo {
+	private Pessoa pessoa;
+	private List<Livro> livros;
+	
+	public Emprestimo(){
+		
+	}
+	
+	
+	
+	
+	
+}
